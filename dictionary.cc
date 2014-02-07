50f1c26ed03418ed2ae1a9357d707a04255b2683
Martin Lööf <mrtn.loof@gmail.com>
2014-02-07 15:06:30 +0100

    Added dictonary construcotr and contains

--- dictionary.cc
+++ dictionary.cc
@@ -5,14 +5,33 @@
 #include <algorithm>
 #include "word.h"
 #include "dictionary.h"
+#include <unordered_set>
 
-using namespace std;
 
 Dictionary::Dictionary() {
+	
+	ifstream wordReader;
+	
+	wordReader.open("words.txt");
+	if(wordReader.fail()){
+		cout<<"Input file could not be opened" << endl;
+	}
+	string word;
+	int nbrTri;
+	while(wordReader >> word >> nbrTri){
+		
+		for( int i = 0 ; i < nbrTri ; ++i){
+			// do something with the trigrams
+		}
+		
+		dict.insert(word);
+		
+	}
+	
 }
 
 bool Dictionary::contains(const string& word) const {
-	return true;
+	return dict.find(word);
 }
 
 vector<string> Dictionary::get_suggestions(const string& word) const {
--- dictionary.h
+++ dictionary.h
@@ -3,6 +3,10 @@
 
 #include <string>
 #include <vector>
+#include <unordered_set>
+
+using namespace std;
+
 
 class Dictionary {
 public:
@@ -10,6 +14,7 @@ public:
 	bool contains(const std::string& word) const;
 	std::vector<std::string> get_suggestions(const std::string& word) const;
 private:
+	unordered_set<string> dict;
 };
 
 #endif
